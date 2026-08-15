83ebc22fa8473213052b74647e70cdda26eac3d6
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2026-08-15 20:05:26 +0300

    redirect root and authed login to scanner page

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ export function middleware(request: NextRequest) {
   }
 
   if (authed && isLoginPage) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/home', request.url));
   }
 
   return NextResponse.next();
